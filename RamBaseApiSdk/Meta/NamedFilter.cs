e0ee64a400e416592ca4f525f4628a4707d0ed3f
Håkon Andrè Førre Knudsen <hakon.andre.knudsen@hatteland.com>
2020-11-05 10:51:42 +0100

    Fixes #5

--- RamBaseApiSdk/Meta/NamedFilter.cs
+++ RamBaseApiSdk/Meta/NamedFilter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using RamBaseApiSdk.Meta;
+using System.Collections.Generic;
 
 namespace RamBase.Api.Sdk.Meta
 {
@@ -7,6 +8,6 @@ namespace RamBase.Api.Sdk.Meta
         public string FilterName { get; set; }
         public string FriendlyFilterName { get; set; }
         public string FilterString { get; set; }
-        public List<string> UncombinableWith { get; set; }
+        public List<UncombinableFilter> UncombinableWith { get; set; }
     }
 }
