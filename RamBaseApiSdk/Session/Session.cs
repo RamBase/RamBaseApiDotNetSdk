8d0bc1108ebdb5ed20765178b276ef316dac4c0b
Håkon Andrè Førre Knudsen <hakon.andre.knudsen@hatteland.com>
2020-11-05 10:52:40 +0100

    Fixes #4

--- RamBaseApiSdk/Session/Session.cs
+++ RamBaseApiSdk/Session/Session.cs
@@ -4,6 +4,7 @@
     {
         public int? Pid { get; set; }
         public int? CustomerId { get; set; }
+        public int? SupplierId { get; set; }
         public int? ContactId { get; set; }
         public string Database { get; set; }
         public string RamBaseName { get; set; }
