36179cf25518d5f9df041fae9d53233929f43aff
Håkon Andrè Førre Knudsen <hakon.andre.knudsen@hatteland.com>
2022-03-28 11:40:21 +0200

    Fix: Only strip & from end of filter if filter has length > 0

--- RamBaseApiSdk/Request/QueryParameters.cs
+++ RamBaseApiSdk/Request/QueryParameters.cs
@@ -3,69 +3,72 @@ using System.Text;
 
 namespace RamBase.Api.Sdk.Request
 {
-    public abstract class QueryParameters
-    {
+	public abstract class QueryParameters
+	{
 
-        /// <summary>
-        /// Set the database to use for the request
-        /// </summary>
-        public string Db { get; set; }
+		/// <summary>
+		/// Set the database to use for the request
+		/// </summary>
+		public string Db { get; set; }
 
-        /// <summary>
-        /// Use to get localized responses. 
-        /// </summary>
-        public string Lang { get; set; }
+		/// <summary>
+		/// Use to get localized responses. 
+		/// </summary>
+		public string Lang { get; set; }
 
-        /// <summary>
-        /// This can be used to test a new version of a resource when your ApiClient is running an older, deprecated, version of a resource. Useminimumversion can be used to test against a newer version of the resource
-        /// </summary>
-        public int? UseMinimumVersion { get; set; }
+		/// <summary>
+		/// This can be used to test a new version of a resource when your ApiClient is running an older, deprecated, version of a resource. Useminimumversion can be used to test against a newer version of the resource
+		/// </summary>
+		public int? UseMinimumVersion { get; set; }
 
-        /// <summary>
-        /// Additional query parameters
-        /// </summary>
-        public Dictionary<string, string> Parameters { get; set; } = new Dictionary<string, string>();
+		/// <summary>
+		/// Additional query parameters
+		/// </summary>
+		public Dictionary<string, string> Parameters { get; set; } = new Dictionary<string, string>();
 
-        /// <summary>
-        /// Add new parameter
-        /// </summary>
-        /// <param name="parameter">Parameter name</param>
-        /// <param name="value">Parameter value</param>
-        public void Add(string parameter, string value)
-        {
-            Parameters.Add(parameter, value);
-        }
+		/// <summary>
+		/// Add new parameter
+		/// </summary>
+		/// <param name="parameter">Parameter name</param>
+		/// <param name="value">Parameter value</param>
+		public void Add(string parameter, string value)
+		{
+			Parameters.Add(parameter, value);
+		}
 
-        /// <summary>
-        /// Creates querystring from set parameters
-        /// </summary>
-        /// <returns>Querystring</returns>
-        public string Build()
-        {
-            return ToString();
-        }
+		/// <summary>
+		/// Creates querystring from set parameters
+		/// </summary>
+		/// <returns>Querystring</returns>
+		public string Build()
+		{
+			return ToString();
+		}
 
-        /// <summary>
-        /// Creates querystring from set parameters
-        /// </summary>
-        /// <returns>Querystring</returns>
-        public override string ToString()
-        {
-            StringBuilder value = new StringBuilder();
+		/// <summary>
+		/// Creates querystring from set parameters
+		/// </summary>
+		/// <returns>Querystring</returns>
+		public override string ToString()
+		{
+			StringBuilder value = new StringBuilder();
 
-            if (!string.IsNullOrEmpty(Db))
-                value.Append($"$db={Db}&");
+			if (!string.IsNullOrEmpty(Db))
+				value.Append($"$db={Db}&");
 
-            if (!string.IsNullOrEmpty(Lang))
-                value.Append($"$lang={Lang}&");
+			if (!string.IsNullOrEmpty(Lang))
+				value.Append($"$lang={Lang}&");
 
-            if (UseMinimumVersion.HasValue)
-                value.Append($"$useMinimumVersion={UseMinimumVersion}&");
+			if (UseMinimumVersion.HasValue)
+				value.Append($"$useMinimumVersion={UseMinimumVersion}&");
 
-            foreach (KeyValuePair<string, string> parameter in Parameters)
-                value.Append($"{parameter.Key}={parameter.Value}&");
+			foreach (KeyValuePair<string, string> parameter in Parameters)
+				value.Append($"{parameter.Key}={parameter.Value}&");
 
-            return value.Remove(value.Length - 1, 1).ToString();
-        }
-    }
+			if (value.Length > 0)
+				value.Remove(value.Length - 1, 1);
+
+			return value.ToString();
+		}
+	}
 }
